d008fd37c9774697712852eb65add9f60ce983c7
rockingrohit9639 <rohitbhatwara1@gmail.com>
2022-07-17 17:21:43 +0530

    added basic Home component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Home from "./Component/Home/Home";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
