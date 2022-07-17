6cc9c42a4bd353dbc5067ea9daac8ec1e026dbaa
rockingrohit9639 <rohitbhatwara1@gmail.com>
2022-07-17 18:27:34 +0530

    fetching user info on button click (listener is not on all buttons)

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import UserCard from "../UserCard/UserCard";
 import "./Home.css";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchUsers } from "../../redux/usersSlice";
+import { fetchUserInfo, fetchUsers } from "../../redux/usersSlice";
 
 function Home() {
   const dispatch = useDispatch();
@@ -12,13 +12,24 @@ function Home() {
     dispatch(fetchUsers());
   }, [dispatch]);
 
+  const handleFetchUserInfo = (e) => {
+    if (e.target.tagName === "BUTTON") {
+      const id = e.target.id;
+      dispatch(fetchUserInfo(id));
+    }
+  };
+
   return (
     <div className="home container">
       <UserCard />
 
-      <div className="home__buttons flex">
+      <div className="home__buttons flex" onClick={handleFetchUserInfo}>
         {Array.from(Array(totalUsers).keys()).map((i) => (
-          <button key={i} className="btn text-secondary ff-montserrat">
+          <button
+            key={i}
+            id={i + 1}
+            className="btn text-secondary ff-montserrat"
+          >
             {i + 1}
           </button>
         ))}
--- src/redux/usersSlice.js
+++ src/redux/usersSlice.js
@@ -10,6 +10,7 @@ const initialState = {
   users: [],
   totalUsers: 0,
   status: STATUSES.IDLE,
+  currentUser: null,
 };
 
 const usersSlice = createSlice({
@@ -28,14 +29,39 @@ const usersSlice = createSlice({
       .addCase(fetchUsers.rejected, (state, action) => {
         state.status = STATUSES.ERROR;
       });
+
+    builder
+      .addCase(fetchUserInfo.pending, (state, action) => {
+        state.status = STATUSES.LOADING;
+      })
+      .addCase(fetchUserInfo.fulfilled, (state, action) => {
+        state.currentUser = action.payload;
+        state.status = STATUSES.IDLE;
+      })
+      .addCase(fetchUserInfo.rejected, (state, action) => {
+        state.status = STATUSES.ERROR;
+      });
   },
 });
 
 export default usersSlice.reducer;
 
-// Users Thunks
+// Users Thunks //
+
+/** Fetch All Users **/
 export const fetchUsers = createAsyncThunk("users/fetch", async () => {
   const res = await fetch("https://reqres.in/api/users");
   const data = await res.json();
   return data;
 });
+
+/** Fetch particular user's info **/
+export const fetchUserInfo = createAsyncThunk(
+  "users/fetchUserInfo",
+  async (id) => {
+    const res = await fetch(`https://reqres.in/api/users/${id}`);
+    const data = await res.json();
+
+    return data.data;
+  }
+);
