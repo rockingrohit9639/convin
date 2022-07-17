91098f5263d611a954fdb0a671815ea8e7793c08
rockingrohit9639 <rohitbhatwara1@gmail.com>
2022-07-17 17:44:45 +0530

    Added Buttons on Home

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -4,8 +4,12 @@ import "./Home.css";
 
 function Home() {
   return (
-    <div className="container">
+    <div className="home container">
       <UserCard />
+
+      <div className="home__buttons flex">
+        <button className="btn text-secondary ff-montserrat">1</button>
+      </div>
     </div>
   );
 }
